5a1e8a8462428e2cdacb04e755e68d3f230db0ee
ittiam <ittiamc@gmail.com>
2019-07-18 14:35:52 +0800

    mix template

---
+++ packages/mix-template-react/.babelrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: 'usage',
+        corejs: 2
+      }
+    ],
+    '@babel/preset-react'
+  ]
+};
---
+++ packages/mix-template-react/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const Index = () => <h1>Hello React !</h1>;
+
+ReactDom.render(<Index />, document.getElementById('app'));
