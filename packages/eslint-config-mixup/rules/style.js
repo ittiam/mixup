ae0185aa069e9594284db11345e8c5f1ff182ca5
czh <ittiamc@gmail.com>
2020-04-15 15:53:21 +0800

    Update style.js

--- packages/eslint-config-mixup/rules/style.js
+++ packages/eslint-config-mixup/rules/style.js
@@ -327,7 +327,7 @@ module.exports = {
 
     // disallow use of unary operators, ++ and --
     // https://eslint.org/docs/rules/no-plusplus
-    'no-plusplus': 'error',
+    'no-plusplus': ['error', { "allowForLoopAfterthoughts": true }],
 
     // disallow certain syntax forms
     // https://eslint.org/docs/rules/no-restricted-syntax
