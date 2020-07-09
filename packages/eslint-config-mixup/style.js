19f33369b7d4642a7556d0ff96205e59e7ed517d
ittiam <ittiamc@gmail.com>
2020-07-09 12:31:43 +0800

    eslint

--- packages/eslint-config-mixup/style.js
+++ packages/eslint-config-mixup/style.js
@@ -429,30 +429,6 @@ module.exports = {
     // require padding inside curly braces
     'object-curly-spacing': ['error', 'always'],
 
-    // enforce line breaks between braces
-    // https://eslint.org/docs/rules/object-curly-newline
-    'object-curly-newline': [
-      'error',
-      {
-        ObjectExpression: {
-          minProperties: 4,
-          multiline: true,
-          consistent: true,
-        },
-        ObjectPattern: { minProperties: 4, multiline: true, consistent: true },
-        ImportDeclaration: {
-          minProperties: 4,
-          multiline: true,
-          consistent: true,
-        },
-        ExportDeclaration: {
-          minProperties: 4,
-          multiline: true,
-          consistent: true,
-        },
-      },
-    ],
-
     // enforce "same line" or "multiple line" on object properties.
     // https://eslint.org/docs/rules/object-property-newline
     'object-property-newline': [
